41d6c2ac10bf2d687380c722642a8f3d3f117db7
Yulia Moshan <juliam12@gmail.com>
2024-08-23 19:08:07 +0300

    added YCbCr class

---
+++ ChromaSubsamplingCompression/YCbCr.cs
@@ -0,0 +1,52 @@
+﻿namespace Image_And_Video_Compression
+{
+    class YCbCr
+    {
+        byte[,] m_Y;
+        byte[,] m_Cb;
+        byte[,] m_Cr;
+
+        public YCbCr(int i_Height, int i_Width, int i_HeightDivider, int i_WidthDivider)
+        {
+            m_Y = new byte[i_Height, i_Width];
+            m_Cb = new byte[i_Height/i_HeightDivider, i_Width/i_WidthDivider];
+            m_Cr = new byte[i_Height/i_HeightDivider, i_Width/i_WidthDivider];
+        }
+
+        public byte[,] Y
+        {
+            get
+            {
+                return m_Y;
+            }
+            set
+            {
+                m_Y = value;
+            }
+        }
+
+        public byte[,] Cb
+        {
+            get
+            {
+                return m_Cb;
+            }
+            set
+            {
+                m_Cb = value;
+            }
+        }
+
+        public byte[,] Cr
+        {
+            get
+            {
+                return m_Cr;
+            }
+            set
+            {
+                m_Cr = value;
+            }
+        }
+    }
+}
